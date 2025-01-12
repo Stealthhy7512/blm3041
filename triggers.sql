3cb5e961a3918f90b7b05c9bfc9379b5de89f48b
kaanyazici <kaanyaz1c1@hotmail.com>
2025-01-12 14:05:29 +0300

    clean up triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,15 +1,10 @@
--- TRIGGERLAR CALISIRKEN MESAJ DONECEK
-
 -- buy_details'e ekleme yaparken urun adinin mevcut olup olmadigini kontrol eden trigger
 create trigger check_name_buy_details_trigger
 before insert on buy_details
 for each row
 execute function check_name_buy_details();
 
--- TODO id ve sifre icin kontrol trigger (gerek yok sanki buna)
-
--- TODO satin alim yaparken stok kontrolu yapan trigger
--- Kontrol et GPT yapti
+-- satin alim yaparken stok kontrolu yapan trigger
 create trigger check_and_update_order_quantity_trigger
 before insert or update on buy
 for each row
